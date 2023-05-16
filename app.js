cae5ebe597d3433c84fc14ecdc1c445ebfeb3374
Sidakveer <sidakveersingh8@gmail.com>
2023-05-15 21:43:10 -0400

    create app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const http = require('http')
-
+const fs = require('fs')
 
 
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    const url = req.url
+    const method = req.method
+    if (url === '/'){
+        res.write('<body> <form method="POST" action="/message" > <input type="text" name="message"> <button type="submit">Send</button> </input> </form> </body>')
+        return res.end();
+    }
+
+    
+    if (url === '/message' && method === "POST"){
+        fs.writeFileSync("message.txt", 'DUmmy')
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+
+    res.setHeader('Content-type', 'text/html')
+    res.write("<h1>Hello people</h1>")
 });
 
 server.listen(3000)
