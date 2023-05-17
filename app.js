bb9417cb28433243e423d4758e182549e82eab55
Sidakveer <sidakveersingh8@gmail.com>
2023-05-17 12:29:31 -0400

    create app.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use("/p", (req, res, next) => {
-    console.log("hhi")
-    res.send("<p>hi</p>")
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).send('page not found')
 })
-
-app.use("/", (req, res, next) => {
-    console.log("f you")
-    res.send("<h2>Hello Babe</h2>")
-    next()
-})
-
-
 
 app.listen(3000);
 
