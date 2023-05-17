cdede4548ce760de2f428efc872b2ce299c0108a
Sidakveer <sidakveersingh8@gmail.com>
2023-05-16 21:11:21 -0400

    create app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const http = require('http');
 const fs = require('fs');
+const express = require('express');
+
+const app = express()
 
 
-const server = http.createServer()
+app.use("/p", (req, res, next) => {
+    console.log("hhi")
+    res.send("<p>hi</p>")
+})
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+    console.log("f you")
+    res.send("<h2>Hello Babe</h2>")
+    next()
+})
+
+
+
+app.listen(3000);
 
 
 
