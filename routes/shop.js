aa9f181aad8721c5740d2b9ad6a95e18d3fa1b1d
Sidakveer <sidakveersingh8@gmail.com>
2023-05-18 12:59:00 -0400

    add routes, views, public, templating engines

--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
-const rootDir = require('../utils/path')
+const path = require('path');
 
+const express = require('express');
 
-router.get("/", (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-})
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
+const router = express.Router();
 
-module.exports = router
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+});
+
+module.exports = router;
