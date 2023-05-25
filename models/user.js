c6c91c82072afd7e55027ed72a9fe1efa6ab3bbd
Sidakveer <sidakveersingh8@gmail.com>
2023-05-25 12:54:35 -0400

    majority_element

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+
+const sequelize = require('../util/database')
+
+const User = sequelize.define('user',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        notNull: true,
+
+    },
+    name: {
+        type: Sequelize.STRING,
+        notNull: true,
+
+    },
+    email: {
+        type: Sequelize.STRING,
+        notNull: true,
+
+    }
+}   
+)
+
+module.exports = User
