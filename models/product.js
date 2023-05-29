96ffcb8d9178cd797246427a5c29ae0fe3133ec0
Sidakveer <sidakveersingh8@gmail.com>
2023-05-29 13:14:40 -0400

    update postCart

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
-const sequelize = require('../util/database')
+const sequelize = require('../util/database');
 
 const Product = sequelize.define('product', {
   id: {
@@ -12,7 +12,7 @@ const Product = sequelize.define('product', {
   title: Sequelize.STRING,
   price: {
     type: Sequelize.DOUBLE,
-    allowNull: false,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
@@ -22,8 +22,6 @@ const Product = sequelize.define('product', {
     type: Sequelize.STRING,
     allowNull: false
   }
+});
 
-})
-
-
-module.exports = Product
+module.exports = Product;
